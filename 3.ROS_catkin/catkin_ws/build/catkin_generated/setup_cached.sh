288bb70b8868845b6a20f895808ccfcac1373556
Chaejin <joung6517@gmail.com>
2021-07-27 23:08:04 +0900

    catkin, package

---
+++ 3.ROS_catkin/catkin_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/cjlotto/git_clone/ROS_Lotto/3.ROS_catkin/catkin_ws/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/cjlotto/git_clone/ROS_Lotto/3.ROS_catkin/catkin_ws/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/cjlotto/git_clone/ROS_Lotto/3.ROS_catkin/catkin_ws/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/cjlotto/git_clone/ROS_Lotto/3.ROS_catkin/catkin_ws/src:$ROS_PACKAGE_PATH"
