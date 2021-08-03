057bbd8fac80063cd698cfae992990262cf56592
Chaejin <joung6517@gmail.com>
2021-08-03 11:13:34 +0900

    Second msg

---
+++ 9.ROS_msg_type/catkin_ws/build/basics/CMakeFiles/basics_generate_messages_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/basics_generate_messages_cpp"
+  "/home/cjlotto/git_clone/ROS_Lotto/9.ROS_msg_type/catkin_ws/devel/include/basics/Complex.h"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/basics_generate_messages_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 9.ROS_msg_type/catkin_ws/devel/share/basics/cmake/basics-msg-paths.cmake
@@ -0,0 +1,4 @@
+# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
+
+set(basics_MSG_INCLUDE_DIRS "/home/cjlotto/git_clone/ROS_Lotto/9.ROS_msg_type/catkin_ws/src/basics/msg")
+set(basics_MSG_DEPENDENCIES std_msgs)
